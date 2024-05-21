133ba45fa9523e2fef7caaa6cfea5e1e93909166
Connor Kress <con2coool@gmail.com>
2024-05-20 23:42:27 -0400

    Generalized vector implementation.

---
+++ src/vector.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+Vec new_vec(size_t item_size) {
+    Vec vec = {
+        .data = NULL,
+        .len = 0,
+        .cap = 0,
+        .item_size = item_size,
+    };
+    return vec;
+}
+
+Vec *new_heap_vec(size_t item_size) {
+    Vec *vec = malloc(sizeof(Vec));
+    vec->data = NULL;
+    vec->len = 0;
+    vec->cap = 0;
+    vec->item_size = item_size;
+    return vec;
+}
+
+void reallocate_vec(Vec *vec, size_t new_cap) {
+    vec->cap = new_cap;
+    void *new_arr = malloc(vec->cap * vec->item_size);
+    memcpy(new_arr, vec->data, vec->len * vec->item_size);
+    free(vec->data);
+    vec->data = new_arr;
+}
+
+void *vec_get(const Vec *vec, size_t idx) {
+    return vec->data + (idx*vec->item_size);
+}
+
+void vec_set(Vec *vec, size_t idx, const void *val) {
+    void *arr_ptr = vec->data + (idx*vec->item_size);
+    memcpy(arr_ptr, val, vec->item_size);
+}
+
+void vec_push(Vec *vec, const void* val) {
+    if (vec->len == vec->cap) {
+        size_t new_cap = (vec->cap == 0) ? 1 : vec->cap * 2;
+        reallocate_vec(vec, new_cap);
+    }
+    vec_set(vec, vec->len, val);
+    vec->len++;
+}
---
+++ src/vector.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+#include <stdlib.h>
+
+typedef struct {
+    void *data;
+    size_t len, cap, item_size;
+} Vec;
+
+Vec new_vec(size_t item_size);
+
+Vec *new_heap_vec(size_t item_size);
+
+void *vec_get(const Vec *vec, size_t idx);
+
+void vec_set(Vec *vec, size_t idx, const void *val);
+
+void vec_push(Vec *vec, const void* val);
+
+#endif // VECTOR_H_
